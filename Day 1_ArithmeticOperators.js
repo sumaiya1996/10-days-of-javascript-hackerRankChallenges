e56f6e28b7853a310031666375a48badb926064f
sumaiya1996 <sumaiya.ubit@gmail.com>
2020-12-22 15:06:44 -0800

    Add files via upload

---
+++ Day 1_ArithmeticOperators.js
@@ -0,0 +1,23 @@
+function getArea(length, width) {
+    let area;
+    area = length*width;
+    // Write your code here
+    
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    perimeter = 2* (length+width);
+    // Write your code here
+    
+    return perimeter;
+}
